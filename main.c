105cb7955080a62fa28caf0798b0190bd5dde8d4
LKETZ <159238584+LKETZ@users.noreply.github.com>
2024-02-23 09:05:17 +0000

    2

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include "node.h"
+#include "queue.h"
+
+void main(int argv,char **argv)
+{
+    
+}
---
+++ node.h
@@ -0,0 +1,9 @@
+typedef struct node{
+   struct node* next;
+   int order_number;
+   int q;
+}order;
+
+typedef struct node Node;
+typedef struct node* NodePtr;+
+
